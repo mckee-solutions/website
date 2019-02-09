c77b6d417f7f9db6cc2a33aa2cb388fff0fa732f
Andy <andrew.l.mckee@gmail.com>
2019-02-09 10:56:51 -0500

    basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {RootComponent} from './components/root/root.component';
+import {LeftSidebarComponent} from './components/left-sidebar/left-sidebar.component';
+import {LogoComponent} from './components/logo/logo.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        RootComponent,
+        LogoComponent,
+        LeftSidebarComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [RootComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/components/root/root.component.ts
+++ src/app/components/root/root.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './root.component.html'
 })
-export class AppComponent {
+export class RootComponent {
   title = 'website';
 }
